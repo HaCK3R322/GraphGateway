34809578f76dc2b9a334e729166252f19dd37ec5
ivanandrosovv <ivanandrosovv@gmail.com>
2025-02-22 18:19:28 +0300

    init

---
+++ src/test/kotlin/stud/ivanandrosovv/diplom/DiplomApplicationTests.kt
@@ -0,0 +1,13 @@
+package stud.ivanandrosovv.diplom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiplomApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
